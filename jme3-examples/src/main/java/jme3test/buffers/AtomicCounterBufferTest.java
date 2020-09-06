ed8e8394e5663b1dcdbc6c2213a20c5cdc2a32ee
Alexander Kasigkeit <a.kasigkeit@web.de>
2020-09-06 13:33:36 +0200

    tests for AtomicCounterBuffers in ComputeShaders. Demonstate order in which invocations are run, one of the tests additionally reads back the final atomic counter value from the GPU and prints it to the console

---
+++ jme3-examples/src/main/java/jme3test/buffers/AtomicCounterBufferTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jme3test.buffers;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.buffer.AtomicCounterBuffer;
+import com.jme3.compute.ComputeShader;
+import com.jme3.compute.ComputeShaderFactory;
+import com.jme3.compute.MemoryBarrierBits;
+import com.jme3.compute.MemoryBarrierBits.MemoryBarrierBit;
+import com.jme3.material.Material;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.shader.VarType;
+import com.jme3.texture.Image;
+import com.jme3.texture.Texture;
+import com.jme3.texture.Texture2D;
+
+/**
+ *
+ * Test to demonstate usage of AtomicCounterBuffers in ComputeShaders. This test
+ * visualizes the order in which ComputeShader invocations are run. Take a look
+ * at https://www.geeks3d.com/20120309/opengl-4-2-atomic-counter-demo-rendering-order-of-fragments/ 
+ * except this test uses ComputeShaders instead of FragmentShaders
+ *
+ * @author Alexander Kasigkeit
+ */
+public class AtomicCounterBufferTest extends SimpleApplication {
+
+    public static void main(String[] args) {
+        AtomicCounterBufferTest t = new AtomicCounterBufferTest();
+        t.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        AtomicCounterBuffer acBuffer = AtomicCounterBuffer.createWithInitialValues(0, 0);
+        Texture2D tex = new Texture2D(512, 512, Image.Format.RGBA16F);
+
+        Geometry geo = new Geometry("testgeo", new Box(1f, 1f, 1f));
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setTexture("ColorMap", tex);
+        geo.setMaterial(mat);
+        rootNode.attachChild(geo);
+
+        ComputeShaderFactory factory = ComputeShaderFactory.create(renderer);
+        ComputeShader shader = factory.createComputeShader(SHADER_SOURCE, "GLSL430");
+        shader.setAtomicCouterBuffer("Counter", acBuffer);
+        shader.setImage("Output", VarType.Texture2D, tex, Texture.Access.WriteOnly, 0, -1, true);
+        shader.run(512, 512, 32, 32, MemoryBarrierBits.from(MemoryBarrierBit.TextureFetch));
+        
+        flyCam.setMoveSpeed(10f);
+    }
+
+    private static final String SHADER_SOURCE = ""
+            + "layout (local_size_x = 32, local_size_y = 32) in;\n"
+            + ""
+            + "layout (binding = 0) uniform atomic_uint m_Counter;\n"
+            + "layout (OUTPUT_FORMAT) uniform writeonly image2D m_Output;\n"
+            + ""
+            + "void main(void) {\n"
+            + "     ivec2 x = ivec2(gl_GlobalInvocationID.xy);\n"
+            + "     uint val = atomicCounterIncrement(m_Counter);\n"
+            + "     imageStore(m_Output, x, vec4(float(val) / (512.0 * 512.0)));\n"
+            + "}\n";
+}
