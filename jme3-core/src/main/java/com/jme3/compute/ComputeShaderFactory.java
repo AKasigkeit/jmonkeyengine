5990e7ad5046375a064e8ea179d0adda8c87022f
Alexander Kasigkeit <a.kasigkeit@web.de>
2020-11-11 19:14:50 +0100

    improved max work group size calculation in ComputeShaderFactory and added power-of-two as well as non-POT versions

--- jme3-core/src/main/java/com/jme3/compute/ComputeShaderFactory.java
+++ jme3-core/src/main/java/com/jme3/compute/ComputeShaderFactory.java
@@ -34,6 +34,8 @@ public class ComputeShaderFactory {
 
     private final Renderer RENDERER;
 
+    private final int[][][] MAX_LOCAL_SIZES = new int[2][3][3];
+
     private final int[] MAX_LOCAL_SIZES_1 = new int[1];
     private final int[] MAX_LOCAL_SIZES_2 = new int[2];
     private final int[] MAX_LOCAL_SIZES_3 = new int[3];
@@ -88,16 +90,14 @@ public class ComputeShaderFactory {
         RENDERER.getLocalWorkGroupSize(shader, store);
     }
 
-    public int[] getMaxLocalSize1D() {
-        return MAX_LOCAL_SIZES_1;
-    }
-
-    public int[] getMaxLocalSize2D() {
-        return MAX_LOCAL_SIZES_2;
-    }
-
-    public int[] getMaxLocalSize3D() {
-        return MAX_LOCAL_SIZES_3;
+    public int getMaxLocalSize(int totalDims, int dim, boolean forcePOT) {
+        if (totalDims < 1 || totalDims > 3) {
+            throw new IllegalArgumentException("totalDims must be between 1 and 3");
+        }
+        if (dim < 1 || dim > 3) {
+            throw new IllegalArgumentException("dim must be between 1 and 3");
+        }
+        return MAX_LOCAL_SIZES[forcePOT ? 1 : 0][totalDims][dim];
     }
 
     private void initMaxLocalSizes() {
@@ -107,77 +107,105 @@ public class ComputeShaderFactory {
         int maxSizeZ = getLimitOrZero(Limits.ComputeShaderMaxWorkGroupSizeZ);
 
         //max local size for 1 dimenstional
-        MAX_LOCAL_SIZES_1[0] = Math.min(maxSizeX, maxInvocations);
+        MAX_LOCAL_SIZES[0][0][0] = Math.min(maxSizeX, maxInvocations);
+        MAX_LOCAL_SIZES[0][0][1] = 1;
+        MAX_LOCAL_SIZES[0][0][2] = 1;
 
-        //max local size for two dimensional
-        int sqrt = (int) Math.sqrt(maxInvocations);
-        int twoDimX;
-        if (isPowerOfTwo(sqrt)) {
-            twoDimX = sqrt;
-        } else {
-            twoDimX = 1;
-            while (twoDimX < sqrt) {
-                twoDimX <<= 1;
-            }
-            twoDimX >>= 1;
+        //max local size for two dimensional 
+        int m2x = getPowerOfTwoSmallerEqual((int) Math.sqrt(maxInvocations));
+        int m2y = maxInvocations / m2x;
+        MAX_LOCAL_SIZES[0][1][0] = Math.min(maxSizeX, Math.max(m2x, m2y));
+        MAX_LOCAL_SIZES[0][1][1] = Math.min(maxSizeY, Math.min(m2x, m2y));
+        MAX_LOCAL_SIZES[0][1][2] = 1;
+
+        int m3x = getPowerOfTwoSmallerEqual((int) Math.pow(maxInvocations, 1.0 / 3.0));
+        int m3y = getPowerOfTwoSmallerEqual(maxInvocations / (m3x * m3x));
+        int m3z = maxInvocations / (m3x * m3y);
+        if (m3x < m3y) {
+            int tmp = m3x;
+            m3x = m3y;
+            m3y = tmp;
         }
-        int twoDimY = maxInvocations / twoDimX;
-        MAX_LOCAL_SIZES_2[0] = Math.min(maxSizeX, twoDimX);
-        MAX_LOCAL_SIZES_2[1] = Math.min(maxSizeY, twoDimY);
+        if (m3y < m3z) {
+            int tmp = m3y;
+            m3y = m3z;
+            m3z = tmp;
+        }
+        if (m3x < m3y) {
+            int tmp = m3x;
+            m3x = m3y;
+            m3y = tmp;
+        }
+        MAX_LOCAL_SIZES[0][2][0] = Math.min(maxSizeX, m3x);
+        MAX_LOCAL_SIZES[0][2][1] = Math.min(maxSizeY, m3y);
+        MAX_LOCAL_SIZES[0][2][2] = Math.min(maxSizeZ, m3z);
 
-        //max local size for three dimensional
-        int thirdrt = thirdRoot(maxInvocations);
-        int threeDimX, threeDimY, threeDimZ;
-        if (isPowerOfTwo(thirdrt)) {
-            threeDimX = thirdrt;
-            threeDimY = thirdrt;
-            threeDimZ = thirdrt;
-        } else {
-            threeDimZ = 1;
-            while (threeDimZ < thirdrt) {
-                threeDimZ <<= 1;
-            }
-            threeDimZ >>= 1;
-            threeDimZ = Math.min(maxSizeZ, threeDimZ);
-            int remainingInvocations = maxInvocations / threeDimZ;
-            int rt = (int) Math.sqrt(remainingInvocations);
-            if (isPowerOfTwo(rt)) {
-                threeDimX = rt;
-                threeDimY = rt;
-            } else {
-                threeDimX = 1;
-                while (threeDimX < rt) {
-                    threeDimX <<= 1;
+        //now calculate power of 2 versions (that are recommended)
+        //for 1D
+        MAX_LOCAL_SIZES[1][0][0] = getPowerOfTwoSmallerEqual(MAX_LOCAL_SIZES[0][0][0]);
+        MAX_LOCAL_SIZES[1][0][1] = 1;
+        MAX_LOCAL_SIZES[1][0][2] = 1;
+
+        //for 2D
+        int p2x = getPowerOfTwoSmallerEqual(MAX_LOCAL_SIZES[0][1][0]);
+        int p2y = getPowerOfTwoSmallerEqual(maxInvocations / p2x);
+        MAX_LOCAL_SIZES[1][1][0] = Math.min(maxSizeX, Math.max(p2x, p2y));
+        MAX_LOCAL_SIZES[1][1][1] = Math.min(maxSizeY, Math.min(p2x, p2y));
+        MAX_LOCAL_SIZES[1][1][2] = 1;
+
+        //and for 3D
+        int p3x = getPowerOfTwoSmallerEqual(MAX_LOCAL_SIZES[0][2][0]);
+        int p3y = getPowerOfTwoSmallerEqual(maxInvocations / (p3x * p3x));
+        int p3z = getPowerOfTwoSmallerEqual(maxInvocations / (p3x * p3y));
+        if (p3x < p3y) {
+            int tmp = p3x;
+            p3x = p3y;
+            p3y = tmp;
+        }
+        if (p3y < p3z) {
+            int tmp = p3y;
+            p3y = p3z;
+            p3z = tmp;
+        }
+        if (p3x < p3y) {
+            int tmp = p3x;
+            p3x = p3y;
+            p3y = tmp;
+        }
+        MAX_LOCAL_SIZES[1][2][0] = Math.min(maxSizeX, p3x);
+        MAX_LOCAL_SIZES[1][2][1] = Math.min(maxSizeY, p3y);
+        MAX_LOCAL_SIZES[1][2][2] = Math.min(maxSizeZ, p3z);
+        /*
+        for (int i = 0; i < 3; i++) {
+            for (int p = 0; p < 2; p++) {
+                System.out.println("DIM " + (i + 1) + " with" + (p == 0 ? "out" : "") + " power of two:");
+                for (int j = 0; j < (i + 1); j++) {
+                    System.out.print(" " + MAX_LOCAL_SIZES[p][i][j]);
                 }
-                threeDimX >>= 1;
-                threeDimX = Math.min(maxSizeX, threeDimX);
-                threeDimY = maxInvocations / (threeDimZ * threeDimX);
-                if (!isPowerOfTwo(threeDimY)) {
-                    int v = 1;
-                    while (v < threeDimY) {
-                        v <<= 1;
-                    }
-                    threeDimY = v;
-                }
+                System.out.println();
             }
         }
-        MAX_LOCAL_SIZES_3[0] = Math.min(maxSizeY, threeDimY);
-        MAX_LOCAL_SIZES_3[1] = Math.min(maxSizeX, threeDimX);
-        MAX_LOCAL_SIZES_3[2] = Math.min(maxSizeZ, threeDimZ);
-
-        //print for debug
-        System.out.println("max invocations: " + maxInvocations + ", maxSizes: (" + maxSizeX + ", " + maxSizeY + ", " + maxSizeZ + ")");
-        System.out.println("MAX LOCAL SIZE 1 : " + Arrays.toString(MAX_LOCAL_SIZES_1));
-        System.out.println("MAX LOCAL SIZE 2 : " + Arrays.toString(MAX_LOCAL_SIZES_2));
-        System.out.println("MAX LOCAL SIZE 3 : " + Arrays.toString(MAX_LOCAL_SIZES_3));
+         */
     }
 
-    private boolean isPowerOfTwo(int n) {
-        return n > 0 && (n & n - 1) == 0;
+    public int[] getMaxLocalSize1D() {
+        return MAX_LOCAL_SIZES[0][0];
     }
 
-    private int thirdRoot(int value) {
-        return (int) Math.pow(value, 1.0 / 3.0);
+    public int[] getMaxLocalSize2D() {
+        return MAX_LOCAL_SIZES[0][1];
+    }
+
+    public int[] getMaxLocalSize3D() {
+        return MAX_LOCAL_SIZES[0][2];
+    }
+
+    private int getPowerOfTwoSmallerEqual(int n) {
+        int p = 1;
+        while (p < n) {
+            p <<= 1;
+        }
+        return p == n ? n : p >> 1;
     }
 
     private int getLimitOrZero(Limits limit) {
